2e329e61dbc6de8fb6386567085429e03a10de3f
ozgeesert <ozgeesert@gmail.com>
2025-04-29 15:02:43 +0300

    Partial Files is changed and edited.

--- app.js
+++ app.js
@@ -11,9 +11,16 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
   res.render("index");
 });
+
+app.get("/about", (req, res) => {
+    res.render("about");
+  });
+
+  app.get("/add", (req, res) => {
+    res.render("add");
+  });
 
 const port = 3000;
 app.listen(port, () => {
